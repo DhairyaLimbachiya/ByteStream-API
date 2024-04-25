48f1fff14278cc3b5b05e54fad5f665fdd827b8a
DhairyaLimbachiya <120327612+DhairyaLimbachiya@users.noreply.github.com>
2024-04-25 10:50:21 +0530

    minor changes

--- byteStream.JobSeeker.API/Utility/Mapping/AutoMapperProfile.cs
+++ byteStream.JobSeeker.API/Utility/Mapping/AutoMapperProfile.cs
@@ -9,10 +9,7 @@ namespace byteStream.JobSeeker.Api.Utility.Mapping
 	{
 		public AutoMapperProfile()
 		{
-			//CreateMap< RegisterRequestDto,ApplicationUser >()
-			//	.ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Email))
-			//	.ForMember(dest => dest.NormalizedUserName, opt => opt.MapFrom(src => src.Email.ToUpper()))
-			//	.ReverseMap();
+			
 
 			CreateMap<AddJobSekerDto, JobSeekers>().ReverseMap();
 			CreateMap<JobSeekerDto, JobSeekers>().ReverseMap();
